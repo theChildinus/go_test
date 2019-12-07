11acb765c67a076d0a02f4c9834e84a36d68de50
theChildinus <zky0825@163.com>
2019-12-07 19:44:01 +0800

    update HFE impl

--- HESample/main.go
+++ HESample/main.go
@@ -7,74 +7,93 @@ import (
 )
 
 type BFVObject struct {
-	params       bfv.Parameters
-	bfvContext   *bfv.BfvContext
-	encoder      *bfv.BatchEncoder
-	kgen         *bfv.KeyGenerator
-	encryptor    *bfv.Encryptor
-	decryptor    *bfv.Decryptor
-	sk           *bfv.SecretKey
-	pk           *bfv.PublicKey
-	encryptorPk  *bfv.Encryptor
-	encryptorSk  *bfv.Encryptor
-	evaluator    *bfv.Evaluator
+	Params       bfv.Parameters
+	BfvContext   *bfv.BfvContext
+	Encoder      *bfv.BatchEncoder
+	Kgen         *bfv.KeyGenerator
+	Encryptor    *bfv.Encryptor
+	Decryptor    *bfv.Decryptor
+	EncryptorPk  *bfv.Encryptor
+	EncryptorSk  *bfv.Encryptor
+	Evaluator    *bfv.Evaluator
 }
 
-type BobKeyStore struct {
-	username     string
-	sk           *bfv.SecretKey
-	switchingkey *bfv.SwitchingKey
+type PublishKeyStore struct {
+	Username     string
+	Sk           *bfv.SecretKey
+	Pk           *bfv.PublicKey
 }
 
-func (b *BFVObject) Init() {
+type SubsrcibeKeyStore struct {
+	Username     string
+	Sk           *bfv.SecretKey
+	Switchingkey *bfv.SwitchingKey
+}
+
+func NewBFVObject(paramsNum int) *BFVObject {
+	b := &BFVObject{}
 	var err error
-	b.params = bfv.DefaultParams[0]
-	b.params.T = 67084289
-	b.bfvContext, err = bfv.NewBfvContextWithParam(&b.params)
+	b.Params = bfv.DefaultParams[paramsNum]
+	b.Params.T = 67084289
+	b.BfvContext, err = bfv.NewBfvContextWithParam(&b.Params)
 	if err != nil {
 		fmt.Println("New BfvContext Error: ", err.Error())
 	}
 
-	b.encoder, err = b.bfvContext.NewBatchEncoder()
+	b.Encoder, err = b.BfvContext.NewBatchEncoder()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	b.Kgen = b.BfvContext.NewKeyGenerator()
+	return b
+}
 
-	b.kgen = b.bfvContext.NewKeyGenerator()
-	b.sk, b.pk = b.kgen.NewKeyPair()
+func NewPublicKeyStore(username string, b *BFVObject) *PublishKeyStore {
+	var err error
+	pks := &PublishKeyStore{}
+	pks.Username = username
+	pks.Sk, pks.Pk = b.Kgen.NewKeyPair()
 
-	b.encryptorPk, err = b.bfvContext.NewEncryptorFromPk(b.pk)
+	b.EncryptorPk, err = b.BfvContext.NewEncryptorFromPk(pks.Pk)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	b.encryptorSk, err = b.bfvContext.NewEncryptorFromSk(b.sk)
+	b.EncryptorSk, err = b.BfvContext.NewEncryptorFromSk(pks.Sk)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	b.evaluator = b.bfvContext.NewEvaluator()
+	b.Evaluator = b.BfvContext.NewEvaluator()
 
 	fmt.Printf("Parameters : N=%d, T=%d, logQ = %d (%d limbs), sigma = %f \n",
-		b.bfvContext.N(), b.bfvContext.T(), b.bfvContext.LogQ(), len(b.params.Qi), b.bfvContext.Sigma())
+		b.BfvContext.N(), b.BfvContext.T(), b.BfvContext.LogQ(), len(b.Params.Qi), b.BfvContext.Sigma())
+	return pks
 }
-func (b *BFVObject) Encrypt(plaintext string) []byte {
 
+func NewSubsrcibeKeyStore(username string, b *BFVObject, pks *PublishKeyStore) *SubsrcibeKeyStore {
+	sks := &SubsrcibeKeyStore{}
+	sks.Username = username
+	sks.Sk = b.Kgen.NewSecretKey()
+
+	bitDecomp := uint64(0)
+	sks.Switchingkey = b.Kgen.NewSwitchingKey(pks.Sk, sks.Sk, bitDecomp)
+	return sks
+}
+
+func Encrypt(b *BFVObject, pks *PublishKeyStore, plaintext string) []byte {
 	bytes := []byte(plaintext)
-	alice := make([]uint64, b.params.N)
+	alice := make([]uint64, b.Params.N)
 	for i := uint64(0); i < uint64(len(bytes)); i++ {
 		alice[i] = uint64(bytes[i])
 	}
-	//alice := make([]uint64, params.N)
-	//for i := uint64(0); i < params.N>>1; i++ {
-	//	alice[i] = i
-	//}
-	alicePlaintext := b.bfvContext.NewPlaintext()
-	_ = b.encoder.EncodeUint(alice, alicePlaintext)
+
+	alicePlaintext := b.BfvContext.NewPlaintext()
+	_ = b.Encoder.EncodeUint(alice, alicePlaintext)
 
 	fmt.Println("Encrypting...")
 
-	aliceCiphertext, err := b.encryptorPk.EncryptNew(alicePlaintext)
+	aliceCiphertext, err := b.EncryptorPk.EncryptNew(alicePlaintext)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -86,26 +105,22 @@ func (b *BFVObject) Encrypt(plaintext string) []byte {
 	return cipherBytes
 }
 
-func (b *BFVObject) Decrypt(ciphertext []byte) string {
-	aliceCiphertext := b.bfvContext.NewCiphertext(1)
+func Decrypt(b *BFVObject, sks *SubsrcibeKeyStore, ciphertext []byte) string {
+	aliceCiphertext := b.BfvContext.NewCiphertext(1)
 	err := aliceCiphertext.UnMarshalBinary(ciphertext)
 	if err != nil {
 		fmt.Println("UnMarshal Failed ", err.Error())
 	}
-	Skbob := b.kgen.NewSecretKey()
-	decryptor_Skbob, err := b.bfvContext.NewDecryptor(Skbob)
+
+	if err := b.Evaluator.SwitchKeys(aliceCiphertext, sks.Switchingkey, aliceCiphertext); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	decryptor_sks, err := b.BfvContext.NewDecryptor(sks.Sk)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
-	bitDecomp := uint64(0)
-	switchingKey := b.kgen.NewSwitchingKey(b.sk, Skbob, bitDecomp)
-
-	if err := b.evaluator.SwitchKeys(aliceCiphertext, switchingKey, aliceCiphertext); err != nil {
-		fmt.Println(err.Error())
-	}
-
-	aliceWant := b.encoder.DecodeUint(decryptor_Skbob.DecryptNew(aliceCiphertext))
+	aliceWant := b.Encoder.DecodeUint(decryptor_sks.DecryptNew(aliceCiphertext))
 	result := make([]byte, len(aliceWant))
 	for i := 0; i < len(aliceWant); i++ {
 		result[i] = byte(aliceWant[i])
@@ -114,23 +129,11 @@ func (b *BFVObject) Decrypt(ciphertext []byte) string {
 	return string(result)
 }
 
-//func (b *BFVObject) GenSwitchKey(username string) {
-//
-//	bobKeyStore := new(BobKeyStore)
-//
-//	decryptor_Skbob, err := b.bfvContext.NewDecryptor(Skbob)
-//	if err != nil {
-//		fmt.Println(err.Error())
-//	}
-//
-//	bitDecomp := uint64(0)
-//	switchingKey := b.kgen.NewSwitchingKey(b.sk, Skbob, bitDecomp)
-//}
-
 func main() {
-	bfvobj := new(BFVObject)
-	bfvobj.Init()
-	ciphertext := bfvobj.Encrypt("hello FHE")
-	plainttext := bfvobj.Decrypt(ciphertext)
+	bfvobj := NewBFVObject(0)
+	pks := NewPublicKeyStore("pub1", bfvobj)
+	sks := NewSubsrcibeKeyStore("sub1", bfvobj, pks)
+	ciphertext := Encrypt(bfvobj, pks, "HELLO HFE")
+	plainttext := Decrypt(bfvobj, sks, ciphertext)
 	fmt.Println(plainttext)
 }
